04f1b032dbc8716a0cc9c7d12432fcc12d5209f0
Majid <49441325+Majid1292@users.noreply.github.com>
2023-05-29 10:20:06 -0400

    working movie model

--- RazorPageMovie/Startup.cs
+++ RazorPageMovie/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPageMovie
 {
@@ -23,6 +24,9 @@ namespace RazorPageMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("RazorPagesMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
